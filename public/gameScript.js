b5aa8d72268b86cee8780ef028c2b6a60dfeba6a
tylerb1 <tberb1290@gmail.com>
2021-03-02 22:46:54 -0800

    change numbers exactly

--- public/gameScript.js
+++ public/gameScript.js
@@ -849,7 +849,7 @@ function moveInnerCircleWavePath() {
   if (innerCircleWavePath) {
     innerCircleWavePath.remove();
   }
-  waveLineY = Math.ceil(innerCirclePath.bounds.bottom - progressToNextPowerUp * innerCirclePath.bounds.height - 2);
+  waveLineY = Math.ceil(innerCirclePath.bounds.bottom - progressToNextPowerUp * innerCirclePath.bounds.height - 3);
   var innerCircleEndpoints = getInnerCircleEndpoints();
   if (innerCircleEndpoints.length === 2) {
     innerCircleWavePathTop = new Path.Line(innerCircleEndpoints[0] + { x: 2, y: 0 }, innerCircleEndpoints[1] - { x: 2, y: 0 });
